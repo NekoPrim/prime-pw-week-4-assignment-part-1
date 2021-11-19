fd18d7175060b5f6120c7178f79de1e3bb6aee7c
Desiree Quade <nekodez@gmail.com>
2021-11-19 02:30:03 -0600

    why am i still awake css

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -112,7 +112,7 @@ function find(value, array) {
       return true;
     }
     if (x != value && x === array[array.length-1]) {
-      console.log(`${value} is not in this array`)
+      console.log(`${value} is not in this array`);
       return false;
     }
   }
@@ -134,12 +134,13 @@ console.log(find('tomato', soups));
 function isFirstLetter(letter, string) {
 
 }
+
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll() {
-  let sum = 0
+  let sum = 0;
   // TODO: loop to add items
   return sum;
 }
